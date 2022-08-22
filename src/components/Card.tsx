9603124ee6ecf498d48b8bda1509577027be1641
PathonScript <pathonscript@gmail.com>
2022-08-22 10:39:17 +0700

    Link on cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,17 +6,18 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core'
 interface Card {
   icon: IconProp,
   name: string,
+  link: string
 }
 
 function Card(props: Card) {
   return (
     <>
-      <div className="rounded-lg bg-orange-300 border-2 border-orange-900 px-2 shadow-md mx-2">
+      <a href={ props.link} className="rounded-lg bg-orange-300 border-2 border-orange-900 px-2 shadow-md mx-2" target="_blank" rel="noreferrer">
         <div className="flex flex-row py-2">
           <FontAwesomeIcon className="flex mx-1 text-orange-900" width={30} icon={props.icon} />
           <h1 className="flex mx-1 poppins text-orange-900 text-2xl">{ props.name }</h1>
         </div> 
-      </div>
+      </a>
     </>
   )
 }
--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -6,9 +6,9 @@ import { faCode } from '@fortawesome/free-solid-svg-icons'
 function Cards() {
   return (
 	<div className="flex flex-row w-1/2 max-h-14 justify-center">
-		<Card icon={faGithub} name="Github" />
-		<Card icon={faTwitter} name="Twitter" />
-		<Card icon={faCode} name="Website" />
+		<Card icon={faGithub} name="Github" link="https://github.com/PathonScript"/>
+		<Card icon={faTwitter} name="Twitter" link="https://twitter.com/Patatotota"/>
+		<Card icon={faCode} name="Website" link="https://patato.live" />
 	</div>
   )
 }
