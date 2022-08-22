872828cbd482796d08788f956e5b12023d22fcdc
PathonScript <pathonscript@gmail.com>
2022-08-22 10:41:01 +0700

    Changed website to email

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Card from './Card';
 import { faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function Cards() {
   return (
 	<div className="flex flex-row w-1/2 max-h-14 justify-center">
 		<Card icon={faGithub} name="Github" link="https://github.com/PathonScript"/>
 		<Card icon={faTwitter} name="Twitter" link="https://twitter.com/Patatotota"/>
-		<Card icon={faCode} name="Website" link="https://patato.live" />
+		<Card icon={faEnvelope} name="Email" link="mailto:pathonscript@gmail.com" />
 	</div>
   )
 }
