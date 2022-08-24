abe2176820ce56886c9d06b878b5c7f79e0c286b
PathonScript <pathonscript@gmail.com>
2022-08-24 17:30:57 +0700

    Updated fonts and icon

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ function About() {
 				  <h1 className="text-5xl title ">About Me🥔</h1>
 				</div>
 			  <div className="flex flex-1 w-full sm:items-center justify-center">
-				<p className="text-3xl poppins text-orange-900">
+				<p className="text-xl sm:text-3xl poppins text-orange-900">
 					  Hi there, I&apos;m Pat. A student from Thailand that is interested in coding, especially in Web Development, Apps, and Bots.    
 					  I am currently studying secondary school at Intertots Trillingual School and code for fun.
 					  I want to hopefully code for a living and move abroad as soon as possible for better life quality and opportunities.
