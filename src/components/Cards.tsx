9164ce0dbc3f815427b1c8e644017b704323e028
PathonScript <pathonscript@gmail.com>
2022-08-24 17:18:50 +0700

    reorder cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Card from './Card';
 import { faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope, faTree } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faTreeCity } from '@fortawesome/free-solid-svg-icons'
 
 function Cards() {
   return (
 	<div className="flex flex-col sm:flex-row w-1/2 justify-center items-center">
 		<Card icon={faGithub} name="Github" link="https://github.com/PathonScript"/>
 		<Card icon={faTwitter} name="Twitter" link="https://twitter.com/Patatotota"/>
+		<Card icon={faTreeCity} name="Garden" link="https://garden.patato.live" />
 		<Card icon={faEnvelope} name="Email" link="mailto:pathonscript@gmail.com" />
-		<Card icon={faTree} name="Garden" link="https://garden.patato.live" />
 		  
 	</div>
   )
