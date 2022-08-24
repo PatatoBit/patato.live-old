57ff5c0b8312687366aca8eda4ba2a56ba401697
PathonScript <pathonscript@gmail.com>
2022-08-24 17:04:56 +0700

    Added garden

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Card from './Card';
 import { faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faTree } from '@fortawesome/free-solid-svg-icons'
 
 function Cards() {
   return (
@@ -9,6 +9,8 @@ function Cards() {
 		<Card icon={faGithub} name="Github" link="https://github.com/PathonScript"/>
 		<Card icon={faTwitter} name="Twitter" link="https://twitter.com/Patatotota"/>
 		<Card icon={faEnvelope} name="Email" link="mailto:pathonscript@gmail.com" />
+		<Card icon={faTree} name="Garden" link="https://garden.patato.live" />
+		  
 	</div>
   )
 }
